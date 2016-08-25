29d3fe0f605159b323733fbe46ef3b7abc5303d3
Qian <pandachain731@gmail.com>
2016-08-24 18:22:17 -0700

    added about section

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by pandachain on 2016-08-24.
+ */
+
+$(window).scroll(function () {
+  var position = $(this).scrollTop();
+
+  $('.section').each(function() {
+    var target = $(this).offset().top + 62;
+
+    var id = $(this).attr('id');
+    // $('.navigation a[href*=id]').removeClass('active');
+
+    if (position >= target) {
+      console.log(11);
+      $('.navigation a[href*=id]').addClass('active');
+
+    }
+  });
+
+});
